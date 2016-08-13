348e7d3da84086c0e699a93bc52bd1a2a3bf7dfc
idoku <dragon.ice.995@hotmail.com>
2016-08-13 13:44:31 +0800

    updaet
    only getword

--- exercises/ch06/6_1.c
+++ exercises/ch06/6_1.c
@@ -4,125 +4,106 @@
 
 #define MAXWORD 100
 
-struct key{
-	char *word;
-	int count;
-} keytab[] = {
-	"auto",0,
-            "break",0,
-            "case",0,
-            "char",0,
-            "const",0,
-            "continue",0,
-            "default",0,
-            /*   ...  */
-            "unsigned",0,
-            "void",0,
-            "volatile",0,
-            "while",0
-};
-
-#define NKEYS (sizeof keytab / sizeof(struct key))
-
 
 int getword(char *,int);
-int binsearch(char *,struct key *,int);
 int getch(void);
 void ungetch(int c);
-int comment();
+
 		
 /* count C keywords */
 int main()
 {	 
 	int n;
 	char word[MAXWORD];
+	 
 	while(getword(word,MAXWORD)!=EOF)
-		if(isalpha(word[0]))
-			if((n=binsearch(word,keytab,NKEYS))>=0)
-				keytab[n].count++;
-	for(n=0;n<NKEYS;n++)
-		if(keytab[n].count>0)
-			printf("%4d %s\n",keytab[n].count,keytab[n].word);
+	{	 
+		printf("%s\n",word);
+	}	
 	return 0;
 }
 
-/* binsearch:find word in tab[0]...tab[n-1] */
-int binsearch(char *word,struct key tab[],int n)
+void delspace(void)
 {
-	int cond;
-	int low,high,mid;
-	low=0;
-	high = n-1;
-	while(low<=high)
+	char state = 0;
+	int c;
+	while(state!=2)
 	{
-		mid = (low+high)/2;
-		if((cond=strcmp(word,tab[mid].word))<0)
-			high = mid-1;
-		else if(cond>0)
-			low = mid+1;
-		else
-			return mid;
+		c = getch();
+		switch(state)
+		{
+			case 0:
+				if(c=='/')
+					state = 1;
+				else if(!isspace(c)){
+					state=2;
+					ungetch(c);
+					}
+				break;
+			case 1:
+				if(c=='*')
+					state =3;
+				else{
+					state=2;					
+					ungetch(c);					
+					ungetch('/');					
+				}
+				break;
+			case 3:
+				if(c=='*'){
+					state = 4;
+				}
+				break;
+			case 4:
+				if(c=='*' || c=='/'){
+					if(c=='/')
+						state =0;
+				}else{
+					state = 3;
+				}
+				break;
+		}
 	}
-	return -1;
 }
 
 /* getwod: get next word or character from inpt */
 int getword(char *word,int lim)
 {
-	int c,d,getch(void);
+	int c,getch(void);
 	void ungetch(int);
-	char *w = word;
-	while(isspace(c=getch()))
-		;
-	if(c!=EOF)
-		*w++=c;
-	if(!isalpha(c) || c =='-' || c=='#')
+	char *w = word,scaped;
+	//del space and comment
+	delspace();		 
+	
+	c = *w++ = getch();
+		
+	if(!isalpha(c) && c!='_' && c!='#' && c!='"')
 	{
-		for(;--lim>0;w++){
-			if(!isalnum(*w=getch()) && *w!='-'){
-				ungetch(*w);
-				break;
-			}
-		}		
+		*w='\0';
+		return c;
 	}
-	else if(c=='\''||c=='\\')
-	{
-		for(;--lim>0;w++){
-			if((*w=getch())=='\\')
-				*++w = getch();
-			else if(*w==c){
-				w++;
-				break;
-			}
-			else if(*w==EOF)
-				break;
+	if(c='"'){
+		for(;w<word+lim&&(isalnum(*w=c=getch())||c=='_');w++)
+			;
+		*w = '\0';
+		ungetch(c);
+		return word[0];
+	}
+	w--;
+	while(w<word+lim-1){
+		if((*w++=c = getch())=='"'&&!scaped)
+			break;
+		scaped = 0;
+		if(c=='\\'){
+			scaped=1;
+			w--;
 		}
-	}
-	else if(c=='/'){
-		if((d=getch())=='*')
-		   c = comment();
-		else
-			ungetch(d);
-	}	
-	*w='\0';
-	return c;
+	}		 
+	*--w='\0';
+	return word[0];
 }
 
-int comment()
-{
-	int c;
-	while((c=getch())!=EOF)
-	{
-		if(c=='*')
-		{
-			if((c=getch())=='/')
-				break;
-		}else{
-			ungetch(c);
-		}
-	}
-	return c;
-}
+
 
 #define BUFSIZE 100
 char buf[BUFSIZE];  /* buffer for ungetch */
