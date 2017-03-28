5c6c64e03be29b9b75f03f9ecfbde075fb85e6db
idoku <dragon.ice.995@hotmail.com>
2017-03-28 23:43:12 +0800

    ch07-6

---
+++ ch07/7_6_cat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void filecopy(FILE *ifp,FILE *ofp);
+
+/* cat: concatenate files,version 2 */
+int main(int argc,char *argv[])
+{
+	FILE *fp;
+	void filecopy(FILE *,FILE *);
+	char *prog = argv[0]; /* program name for error */
+	if(argc == 1) /* no args; copy standard input */
+		filecopy(stdin,stdout);
+	else
+		while(--argc>0){
+			if((fp = fopen(*++argv,"r")) == NULL){
+				fprintf(stderr,"%s:can't open %s\n",prog,*argv);
+				exit(1);
+			}else{
+				filecopy(fp,stdout);
+				fclose(fp);
+			}			
+		}
+	if(ferror(stdout)){
+		fprintf(stderr,"%s:error writing stdout\n",prog);
+	}
+	exit(0);
+	return 0;
+}
+
+/* filecopy:copy file ifp to file ofp */
+void filecopy(FILE *ifp,FILE *ofp)
+{
+	int c;
+	while((c=getc(ifp))!=EOF)
+		putc(c,ofp);
+}
